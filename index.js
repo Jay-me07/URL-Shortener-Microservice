db0ddd57b8949be50888fd76c0cc5571755f21a1
Jay-me07 <jnjihiagachau07@gmail.com>
2025-05-30 20:34:21 +0300

    Complete URL Shortener

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+let bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,6 +19,42 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+const urlDatabase = {};
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post('/api/shorturl', function(req, res) {
+  const { url } = req.body;
+
+  //URL validation to check if starts with http or https
+  if(!url.includes('http://') && !url.includes('https://')) {
+    return res.json({ error: 'invalid url' })
+  }
+
+  //Generate a short id
+  const shortId = Math.floor(Math.random() * 1000).toString();
+
+  //Store the url
+  urlDatabase[shortId] = url;
+
+  res.json({
+    original_url: url,
+    short_url: shortId
+  })
+})
+
+app.get('/api/shorturl/:shortId', function(req, res) {
+  const { shortId } = req.params;
+  const original_url = urlDatabase[shortId];
+
+  if(original_url){
+    res.redirect(original_url);
+  } else {
+    res.json({ error: 'invalid url' });
+  }
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
